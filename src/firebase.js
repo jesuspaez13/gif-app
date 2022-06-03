06e901af2de7bdc63a466a456f8f2536a78847d1
Jesus Paez <jpaez13@cuc.edu.co>
2022-06-03 16:38:11 -0500

    Commit final

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLXZq_hFHE2TJi6UPJdaErEk-0nkLAGoA",
+  authDomain: "gif-app-125c5.firebaseapp.com",
+  projectId: "gif-app-125c5",
+  storageBucket: "gif-app-125c5.appspot.com",
+  messagingSenderId: "875401425761",
+  appId: "1:875401425761:web:008b0b290616406ac58b20"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
